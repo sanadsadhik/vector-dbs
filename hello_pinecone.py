71ec75a04dd051e775fc0c6e965f2ea3a0a428fe
sanadsadhik <56155278+sanadsadhik@users.noreply.github.com>
2024-11-29 18:18:49 +0530

    .gitignore and intro to pinecone

---
+++ hello_pinecone.py
@@ -0,0 +1,19 @@
+from pinecone import Pinecone, ServerlessSpec
+from config import PINECONE_API_KEY
+
+api_key = PINECONE_API_KEY
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+# to list all the indexes
+# print(pc.list_indexes())
+
+pc.create_index(
+    name="test",
+    dimension=3,
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+) 
+# "us-east-1" - for free tier use this region only
+print(pc.list_indexes())
